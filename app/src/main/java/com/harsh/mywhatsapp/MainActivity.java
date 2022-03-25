f32b019166bccbff486e846dfaba0bbb0481b8de
Harsh <73978943+Harsh786786@users.noreply.github.com>
2022-03-25 17:02:36 +0530

    First Commit Before FireBase

--- app/src/main/java/com/harsh/mywhatsapp/MainActivity.java
+++ app/src/main/java/com/harsh/mywhatsapp/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-        TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
