67adcec63e06a4ad51de7a23d75a76be803a7553
Harsh <73978943+Harsh786786@users.noreply.github.com>
2022-03-25 16:43:03 +0530

    Seacond commit after textView

--- app/src/main/java/com/harsh/mywhatsapp/MainActivity.java
+++ app/src/main/java/com/harsh/mywhatsapp/MainActivity.java
@@ -3,9 +3,10 @@ package com.harsh.mywhatsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+        TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
